a5bbf4f99f30704f5bc9acb347255823109528c5
Hao Shen <38788766+it-hao@users.noreply.github.com>
2023-02-19 21:19:19 +0800

    Delete code/model directory

--- code/model/dcn/build/lib.linux-x86_64-3.6/modules/__init__.py
+++
@@ -1,3 +0,0 @@
-from .deform_conv import DeformConv, _DeformConv, DeformConvPack
-from .modulated_deform_conv import ModulatedDeformConv, _ModulatedDeformConv, ModulatedDeformConvPack
-from .deform_psroi_pooling import DeformRoIPooling, _DeformRoIPooling, DeformRoIPoolingPack
--- code/model/dcn/build/lib.linux-x86_64-3.7/functions/__init__.py
+++
@@ -1,3 +0,0 @@
-from .deform_conv_func import DeformConvFunction
-from .modulated_deform_conv_func import ModulatedDeformConvFunction
-from .deform_psroi_pooling_func import DeformRoIPoolingFunction
