2ec84586004120dd76d64cd984dab3336d112d8c
Hao Shen <38788766+it-hao@users.noreply.github.com>
2023-02-19 21:22:05 +0800

    Add files via upload

---
+++ train_afdnet_n15.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python main.py --n_GPUs 1 --model adfnet --save adfnet_n15 --noiseL 15 --patch_size 128 --loss 1*MSE --lr 1e-4 --save_results 
---
+++ train_afdnet_n25.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python main.py --n_GPUs 1 --model adfnet --save adfnet_n25 --noiseL 25 --patch_size 128 --loss 1*MSE --lr 1e-4 --save_results 
